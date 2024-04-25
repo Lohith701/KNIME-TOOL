f933c449877d6cc4c755ead880b2fed87219c965
Lohith701 <146410468+Lohith701@users.noreply.github.com>
2024-04-25 12:36:17 +0530

    digital Assignment-2

---
+++ agnes.r
@@ -0,0 +1,21 @@
+# Load the dataset
+healthcare_data <- read.csv("C:/Users/LOHITH/Downloads/archive (2)/healthcare_dataset.csv")
+
+# Select the categorical features for clustering
+categorical_data <- healthcare_data[, c("Name", "Gender", "Blood.Type", "Medical.Condition", "Doctor", "Hospital", "Insurance.Provider", "Admission.Type", "Medication", "Test.Results")]
+
+# Convert categorical variables to factors
+categorical_data <- lapply(categorical_data, as.factor)
+
+# Convert to dataframe
+categorical_df <- as.data.frame(categorical_data)
+
+# Compute dissimilarity matrix using Gower's distance
+library(cluster)
+dissimilarity_matrix <- daisy(categorical_df, metric = "gower")
+
+# Perform hierarchical clustering using AGNES
+agnes_result <- agnes(dissimilarity_matrix)
+
+# Plot the dendrogram
+plot(agnes_result, main = "Dendrogram: Hierarchical Clustering (AGNES)")
